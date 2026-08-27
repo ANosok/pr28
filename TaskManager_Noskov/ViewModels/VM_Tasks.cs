00830a71908ce12c020ed21bd9d6da5f71c724d7
oskar1269 <fahrutdinovoskar4@gmail.com>
2026-08-27 11:48:04 +0500

    Разработанная модель представления задач. Носков

--- TaskManager_Noskov/ViewModels/VM_Tasks.cs
+++ TaskManager_Noskov/ViewModels/VM_Tasks.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager_Noskov.Classes;
 using TaskManager_Noskov.Context;
 using TaskManager_Noskov.Models;
 
 namespace TaskManager_Noskov.ViewModels
 {
-    public class VM_Tasks
+    public class VM_Tasks : Notification
     {
+        public ObservableCollection<Tasks> Tasks { get; set; }
         public TasksContext tasksContext;
-        public List<Tasks> Tasks {  get; set; }
+        public VM_Tasks() =>
+            Tasks = new ObservableCollection<Tasks>(tasksContext.Tasks.OrderBy(x=>x.Done));
+
+        public RealyCommand OnAddTask
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    Tasks newTask = new Tasks()
+                    {
+                        DateExecute = DateTime.Now
+                    };
+                    Tasks.Add(newTask);
+                    tasksContext.Tasks.Add(newTask);
+                    tasksContext.SaveChanges();
+                });
+
+            }
+            
+        }
     }
 }
